0149d7dcf1e049c0e3902696b30c49d5831b27cb
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2025-12-27 23:55:08 +0530

    overall structure

--- programs/solana_predication_market/src/lib.rs
+++ programs/solana_predication_market/src/lib.rs
@@ -3,14 +3,26 @@ use anchor_lang::prelude::*;
 declare_id!("J3kZR7H8pqE67JELXpq4BRP7Ws7JWDCvxHE5UwVZyw2L");
 
 #[program]
-pub mod solana_predication_market {
+pub mod prediction_market {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_market() -> Result<()> {
+        Ok(())
+    }
+
+    pub fn place_bet() -> Result<()> {
+        Ok(())
+    }
+
+    pub fn settle_market() -> Result<()> {
+        Ok(())
+    }
+
+    pub fn claim_reward() -> Result<()> {
+        Ok(())
+    }
+
+    pub fn cancel_market() -> Result<()> {
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
